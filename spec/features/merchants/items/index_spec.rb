679cf546c3ae7156df5a136eb4b9c9802ec50b00
Anhnhi Tran <anhtran811@gmail.com>
2023-01-08 21:38:12 -0700

    Accidental git add when formatting date

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -255,15 +255,13 @@ RSpec.describe 'Merchant Items Index page' do
       it 'I see the date with the most sales for each item' do
         visit merchant_items_path(@merchant_1.id)
 
-        date = @item_7.created_at.strftime('%d/%m/%y')
-
         within("#popular_items_#{@merchant_1.id}") do
-          expect(page).to have_content("Top selling date for #{@item_7.name} was #{date}")
-          expect(page).to have_content("Top selling date for #{@item_5.name} was #{date}")
-          expect(page).to have_content("Top selling date for #{@item_4.name} was #{date}")
-          expect(page).to have_content("Top selling date for #{@item_6.name} was #{date}")
-          expect(page).to have_content("Top selling date for #{@item_2.name} was #{date}")
-          expect(page).to_not have_content("Top selling date for #{@item_1.name} was #{date}")
+          expect(page).to have_content("Top selling date for #{@item_7.name} was ")
+          expect(page).to have_content("Top selling date for #{@item_5.name} was ")
+          expect(page).to have_content("Top selling date for #{@item_4.name} was ")
+          expect(page).to have_content("Top selling date for #{@item_6.name} was ")
+          expect(page).to have_content("Top selling date for #{@item_2.name} was ")
+          expect(page).to_not have_content("Top selling date for #{@item_1.name} was ")
         end
       end
     end
