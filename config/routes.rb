770f0572c4d93be3c74b4d3dcf4c33c20a5d3286
Anhnhi Tran <anhtran811@gmail.com>
2023-01-10 12:45:30 -0700

    Adds welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   #merchants
+  get '/', to: 'application#welcome'
   get '/merchants/:id/dashboard', to: 'merchants#show'
   # resources :merchants, only: [:show] do 
   #   resources :dashboard, controller: "merchants"
---
+++ spec/features/welcome/welcome_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome Page' do
+  it 'displays a welcome page' do
+    visit '/'
+
+    expect(page).to have_content("Welcome to Little Esty Shop")
+    expect(page).to have_content("Collaborators")
+    expect(page).to have_content("Leo Banos Garcia")
+    expect(page).to have_content("Matisse Mallette")
+    expect(page).to have_content("Kelsie Porter")
+    expect(page).to have_content("Anhnhi Tran")
+  end
+end
