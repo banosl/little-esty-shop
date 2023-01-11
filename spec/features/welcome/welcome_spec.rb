467b832bb97e12cff5113a4fe96689e50e91e83e
Kelsie Porter <krporter22@gmail.com>
2023-01-11 12:24:51 -0700

    Update API tests

--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -1,30 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome Page' do
-  it 'displays a welcome page' do
+  it 'displays repo name, users, total commits, and merged pull request from API call' do
     visit '/'
 
-    expect(page).to have_content("Welcome to Little Esty Shop")
-    expect(page).to have_content("Collaborators")
-    expect(page).to have_content("Leo Banos Garcia")
-    expect(page).to have_content("Matisse Mallette")
-    expect(page).to have_content("Kelsie Porter")
-    expect(page).to have_content("Anhnhi Tran")
-  end
-
-  it 'displays total commits from API call' do
-    visit '/'
-
+    expect(page).to have_content("Git Repo Name: ")
     expect(page).to have_content("MatisseMallette Total Commits: ")
     expect(page).to have_content("KelsiePorter Total Commits: ")
     expect(page).to have_content("banosl Total Commits: ")
     expect(page).to have_content("anhtran811 Total Commits: ")
+    expect(page).to have_content("Total Merged Pull Requests ")
 
     visit '/admin'
 
+    expect(page).to have_content("Git Repo Name: ")
     expect(page).to have_content("MatisseMallette Total Commits: ")
     expect(page).to have_content("KelsiePorter Total Commits: ")
     expect(page).to have_content("banosl Total Commits: ")
     expect(page).to have_content("anhtran811 Total Commits: ")
+    expect(page).to have_content("Total Merged Pull Requests: ")
   end
 end
